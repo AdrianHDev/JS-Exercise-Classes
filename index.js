d268279e8b1fbbd79d5100c1c5b56df0d19eda07
adrian <adrianhensondev@gmail.com>
2021-04-22 14:54:47 -0400

    Extend Class Instructor with Lambdasian Class.
    Create Lambdasian constructor with lambdasian argument.
        - Super lambdasian argument
        - Set Instructor's specialty, favLanguage, and catchPhrase to
        lambdasian key values.
    
    Create demo method with subject argument.
        - Return requested string with demo's subject argument.
    
    Create grade method with student and subject argument.
        - Return student's name, and subject in requested string.

--- index.js
+++ index.js
@@ -139,8 +139,19 @@ class Airplane {
           + `demo` receives a `subject` string as an argument and returns the phrase 'Today we are learning about {subject}' where subject is the param passed in.
           + `grade` receives a `student` object and a `subject` string as arguments and returns '{student.name} receives a perfect score on {subject}'
   */
- class Instructor {
-
+ class Instructor extends Lambdasian {
+   constructor(lambdasian) {
+     super(lambdasian);
+     this.specialty = lambdasian.specialty;
+     this.favLanguage = lambdasian.favLanguage;
+     this.catchPhrase = lambdasian.catchPhrase;
+   }
+   demo(subject) {
+     return `Today we are learning about ${subject}`;
+   }
+   grade(student, subject) {
+     return `${student.name} receives a perfect score on ${subject}`
+   }
  }
   /*
     TASK 5
